8f6046f5f202e7d1337f90be86b462376699a0ed
Yanis Adekalom <yanis971.60@gmail.com>
2023-08-01 13:45:27 +0200

    Initial commit

---
+++ src/main/java/fr/dawan/demogitbranching/DemoGitBranchingApplication.java
@@ -0,0 +1,13 @@
+package fr.dawan.demogitbranching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitBranchingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGitBranchingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/dawan/demogitbranching/DemoGitBranchingApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.dawan.demogitbranching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitBranchingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
