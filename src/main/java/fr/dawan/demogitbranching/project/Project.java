bf7f97aa792d8d7c7b21ce34adf729eca7d42156
Yanis Adekalom <yanis971.60@gmail.com>
2023-08-01 13:48:54 +0200

    Feat(database) : Project Entity Created

---
+++ src/main/java/fr/dawan/demogitbranching/project/Project.java
@@ -0,0 +1,23 @@
+package fr.dawan.demogitbranching.project;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Version
+    private Integer version;
+
+    private String name;
+}
